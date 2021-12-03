2c3d04d754389574fbdc62ec6b0f5344c0f90de4
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-12-03 21:23:40 +0100

    Day 2

--- app/solutions/2.ts
+++ app/solutions/2.ts
@@ -1,3 +1,23 @@
 export default (input: string): string => {
-  return '150'
+  let horizontalPostion = 0
+  let depth = 0
+  let aim = 0
+
+  const lines = input.split('\n').filter((line) => line.trim().length > 0)
+
+  const calculations: Record<string, (x: number) => void> = {
+    forward: (x: number) => {
+      horizontalPostion += x
+      depth += aim * x
+    },
+    down: (x: number) => (aim += x),
+    up: (x: number) => (aim -= x),
+  }
+
+  for (const line of lines) {
+    const [command, value] = line.split(' ')
+    calculations[command](parseInt(value))
+  }
+
+  return (horizontalPostion * depth).toString()
 }
