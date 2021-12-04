c3df666e38e0c8057973b353045c5ed0c8c7f508
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-12-04 22:17:42 +0100

    Day 2: Split part 1 and 2

--- app/solutions/2.ts
+++ app/solutions/2.ts
@@ -1,5 +1,31 @@
-export default (input: string): string => {
-  let horizontalPostion = 0
+import { Solution } from '~/types'
+
+export default (input: string): Solution => {
+  return { 'Part 1': getPart1(input), 'Part 2': getPart2(input) }
+}
+
+const getPart1 = (input: string): string => {
+  let horizontalPosition = 0
+  let depth = 0
+
+  const lines = input.split('\n').filter((line) => line.trim().length > 0)
+
+  const calculations: Record<string, (x: number) => void> = {
+    forward: (x: number) => (horizontalPosition += x),
+    down: (x: number) => (depth += x),
+    up: (x: number) => (depth -= x),
+  }
+
+  for (const line of lines) {
+    const [command, value] = line.split(' ')
+    calculations[command](parseInt(value))
+  }
+
+  return (horizontalPosition * depth).toString()
+}
+
+const getPart2 = (input: string): string => {
+  let horizontalPosition = 0
   let depth = 0
   let aim = 0
 
@@ -7,7 +33,7 @@ export default (input: string): string => {
 
   const calculations: Record<string, (x: number) => void> = {
     forward: (x: number) => {
-      horizontalPostion += x
+      horizontalPosition += x
       depth += aim * x
     },
     down: (x: number) => (aim += x),
@@ -19,5 +45,5 @@ export default (input: string): string => {
     calculations[command](parseInt(value))
   }
 
-  return (horizontalPostion * depth).toString()
+  return (horizontalPosition * depth).toString()
 }
