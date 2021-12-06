e92abd3b7c6a8e3b29c3d35813bfe4b035efe931
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-12-06 21:51:45 +0100

    Day 3 - part 2

--- app/solutions/day-3/index.ts
+++ app/solutions/day-3/index.ts
@@ -44,5 +44,47 @@ const toDecimal = (binary: string): number => {
 }
 
 const getPart2 = (input: string): string => {
-  return '-1'
+  const lines = input.split('\n').filter((line) => line.trim().length > 0)
+
+  const filter = (
+    toFilter: string[],
+    keepZeroesWhen: (a: number, b: number) => boolean,
+  ) => {
+    const zeroes = toFilter.filter(
+      (line) => line.substring(position, position + 1) === '0',
+    )
+    const ones = toFilter.filter(
+      (line) => line.substring(position, position + 1) === '1',
+    )
+
+    return keepZeroesWhen(ones.length, zeroes.length) ? zeroes : ones
+  }
+
+  // oxygen generator rating
+  let position = 0
+  let most = [...lines]
+  for (let i = 0; i < lines.length; i++) {
+    most = filter(
+      most,
+      (howManyOnes, howManyZeroes) => howManyOnes < howManyZeroes,
+    )
+    if (most.length === 1) break
+    position++
+  }
+
+  // CO2 scrubber rating
+  position = 0
+  let least = [...lines]
+  for (let i = 0; i < lines.length; i++) {
+    least = filter(
+      least,
+      (howManyOnes, howManyZeroes) => howManyOnes >= howManyZeroes,
+    )
+    if (least.length === 1) break
+    position++
+  }
+
+  const solution = toDecimal(most[0]) * toDecimal(least[0])
+
+  return solution.toString()
 }
